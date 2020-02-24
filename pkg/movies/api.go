52fb0ef098a2df0eee75f0da075f40b4c0c7f24a
johndoe <johndoe@localhost.localdomain>
2020-02-24 13:53:11 -0600

    Changes made for sorted-movies and 100 page capability

--- pkg/movies/api.go
+++ pkg/movies/api.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
 )
 
 // APIMovieSearcher is a MovieSearcher implementation using omdbapi
@@ -20,24 +21,34 @@ type omdbapiResponse struct {
 // SearchMovies searches for a movie
 func (s *APIMovieSearcher) SearchMovies(query string) ([]Movie, error) {
 
-	// call omdbapi
-	params := url.Values{}
-	params.Add("s", query)
-	params.Add("apikey", s.APIKey)
-	params.Add("type", "movie")
-	resp, err := http.Get(s.URL + "?" + params.Encode())
-	if err != nil {
-		return nil, err
-	}
+	const maxPagesPerRequest = 100
+	var sliceOfMovies []Movie
+	for i := 1; i <= maxPagesPerRequest; i++ {
+		// call omdbapi
+		params := url.Values{}
+		params.Add("s", query)
+		params.Add("apikey", s.APIKey)
+		params.Add("type", "movie")
+		params.Add("page", strconv.Itoa(i))
+		resp, err := http.Get(s.URL + "?" + params.Encode())
+		if err != nil {
+			return nil, err
+		}
 
-	// unmarshall response and get the movie array
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	var respStruct omdbapiResponse
-	json.Unmarshal(respBody, &respStruct)
+		// unmarshall response and get the movie array
+		respBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		var respStruct omdbapiResponse
+		json.Unmarshal(respBody, &respStruct)
 
+		if respStruct.Search == nil {
+			break
+		} else {
+			sliceOfMovies = append(sliceOfMovies, respStruct.Search...)
+		}
+	}
 	// return result
-	return respStruct.Search, nil
+	return sliceOfMovies, nil
 }
