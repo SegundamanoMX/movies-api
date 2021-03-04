9d7f81f5fd06fd41d1fb5e4bc3577bd0089a5438
Edgar Fuentes Ramirez <E001215@proveedor.teco.com.ar>
2021-03-04 00:05:39 -0600

    /movies-sorted endpoint was added

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,12 +3,14 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"sort"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.mpi-internal.com/guillermo-dlsg/movies-api/pkg/movies"
 )
 
-func createSearchMoviesHandler(s movies.MovieSearcher) func(http.ResponseWriter, *http.Request) {
+func createSearchMoviesHandler(s movies.MovieSearcher, sortAscending bool) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
 		// get parameter from request
 		queryParams := req.URL.Query()
@@ -16,6 +18,17 @@ func createSearchMoviesHandler(s movies.MovieSearcher) func(http.ResponseWriter,
 
 		// call service to get movies
 		movies, err := s.SearchMovies(searchQuery)
+
+		// ascending sort if true
+		if sortAscending {
+			sort.SliceStable(movies, func(i, j int) bool {
+				if strings.Compare(movies[i].Year, movies[j].Year) == 0 {
+					return movies[i].Title < movies[j].Title
+				}
+
+				return movies[i].Year < movies[j].Year
+			})
+		}
 
 		// generate response
 		response := make(map[string]interface{})
@@ -30,6 +43,9 @@ func createSearchMoviesHandler(s movies.MovieSearcher) func(http.ResponseWriter,
 // NewHandler returns a router with all endpoint handlers
 func NewHandler(s movies.MovieSearcher) http.Handler {
 	router := mux.NewRouter()
-	router.HandleFunc("/movies", createSearchMoviesHandler(s)).Methods("GET")
+
+	router.HandleFunc("/movies", createSearchMoviesHandler(s, false)).Methods("GET")
+	router.HandleFunc("/movies-sorted", createSearchMoviesHandler(s, true)).Methods("GET")
+
 	return router
 }
