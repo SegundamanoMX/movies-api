62d1a11fc46307560ee0ed921bd2fb46dd0a62ca
Jorge Villanueva <jorge.villanueva.87@gmail.com>
2022-07-01 01:50:31 -0500

    feature: add new endpoint movies-sorted and add optional queryparam for pagination

--- pkg/movies/api_test.go
+++ pkg/movies/api_test.go
@@ -45,7 +45,7 @@ func TestSearchMovies(t *testing.T) {
 
 		// run test
 		t.Run(c.name, func(t *testing.T) {
-			actualMovies, actualError := searcher.SearchMovies("star wars")
+			actualMovies, actualError := searcher.SearchMovies("star wars", "1")
 			assert.EqualValues(t, c.expectedMovies, actualMovies)
 			if c.expectedErrorString == "" {
 				assert.NoError(t, actualError)
